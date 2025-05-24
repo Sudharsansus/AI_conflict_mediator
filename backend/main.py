65eba185c55cbbc24c51f6be4fb946875478c1df
Sudharsansus <sudharsan.e585@gmail.com>
2025-05-24 22:34:17 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
-# backend/main.py
-
 from fastapi import FastAPI
 from backend.api import conflict
+from backend.api import admin  # 👈 Add this
 
 app = FastAPI()
+
 app.include_router(conflict.router)
+app.include_router(admin.router, prefix="/admin")  # 👈 Add this
